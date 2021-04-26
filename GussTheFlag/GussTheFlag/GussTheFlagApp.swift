7e3c0410ab5d4c2416a322b2e130d1f56e49c9a2
myang <mu.yang@gmail.com>
2021-04-26 23:38:57 +0300

    Initial version.

---
+++ GussTheFlag/GussTheFlag/GussTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GussTheFlagApp.swift
+//  GussTheFlag
+//
+//  Created by Mu Yang on 26.4.2021.
+//
+
+import SwiftUI
+
+@main
+struct GussTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
