940bb48ca7f877d557d46ff0854a20bf61fb5749
myang <mu.yang@gmail.com>
2021-05-01 23:40:29 +0300

    add flag button tapped.

--- GussTheFlag/GussTheFlag/ContentView.swift
+++ GussTheFlag/GussTheFlag/ContentView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
@@ -26,7 +29,7 @@ struct ContentView: View {
                 ForEach(0 ..< 3) { number in
                     Button(action: {
                         //flag was tapped
-                        
+                        self.flagTapped(number)
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
@@ -35,6 +38,24 @@ struct ContentView: View {
                 Spacer()
             }
         }
+        .alert(isPresented: $showingScore) {
+            Alert(title: <#T##Text#>(scoreTitle), message: Text?Text("Your score is ???"),, dismissButton: <#T##Alert.Button?#>)
+        }
+    }
+    
+    func flagTapped(_ num: Int) {
+        if num == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffled()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
