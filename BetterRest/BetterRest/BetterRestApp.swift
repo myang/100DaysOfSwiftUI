7cc4d81f42e4114673d8f0542cfdf5e87707d47a
minusta <mu.yang@gmail.com>
2021-05-11 22:09:21 +0300

    Initial version.

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mu Yang on 11.5.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
