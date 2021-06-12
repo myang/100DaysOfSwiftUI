23c78e3809d9a1ca8467885ba559fcc72e832ac6
minusta <mu.yang@gmail.com>
2021-06-12 18:27:49 +0300

    add a question counter.

--- NineNineTable/NineNineTable/ContentView.swift
+++ NineNineTable/NineNineTable/ContentView.swift
@@ -10,15 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State private var settings = true
     @State private var level = 12
-    @State private var questions = 5
+    @State private var questionNum = 5
     @State private var answer = "0"
-    @State private var num1 = Int.random(in: 0...2)
-    @State private var num2 = Int.random(in: 0...2)
-    @State private var counter = 0
+    @State private var num1 = 0
+    @State private var num2 = 0
     @State private var buttonAnimation = false
     @State private var enableAlert = false
     @State private var alertTitle = "Correct"
     @State private var alertMsg = ""
+    @State private var counter = 0
 
     var body: some View {
         Group {
@@ -28,7 +28,7 @@ struct ContentView: View {
                             Text("Level number: \(level)")
                     }
 
-                    Picker("Number of questions:", selection: $questions) {
+                    Picker("Number of questions:", selection: $questionNum) {
                         ForEach(1 ..< 5) {
                             Text("\($0 * 5)")
                         }
@@ -36,28 +36,34 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                     
                     Button("Ok") {
-                        settings = false
                         withAnimation {
                             buttonAnimation.toggle()
                         }
+                        num1 = Int.random(in: 0...level)
+                        num2 = Int.random(in: 0...level)
+
+                        settings = false
                     }
                     .frame(width: 100, height: 50)
-                    .background(buttonAnimation ? Color.blue : Color.red)
+                    .background(Color.red)
                     .foregroundColor(.white)
                     .clipShape(RoundedRectangle(cornerRadius: 30))
                     .rotation3DEffect(.degrees(buttonAnimation ? 360 : 0), axis: (x: 0, y: 1, z: 0))
 
                 }
             } else {
-                VStack {
-                    Section (header: Text("\(num1) x \(num2) = ?")) {
-                        TextField("Answer: \(answer)", text: $answer, onCommit: getResult)
-                            .keyboardType(.numberPad)
-                            .textFieldStyle(RoundedBorderTextFieldStyle()).autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/).padding()
-                    }
+                HStack {
+                    Text("\(num1) x \(num2) =")
+                        .font(.headline)
+                    TextField("Answer: \(answer)", text: $answer, onCommit: getResult)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle()).autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/).padding()
                 }
                 .alert(isPresented: $enableAlert) {
-                    Alert(title: Text(alertTitle), message: Text(alertMsg), dismissButton: .default(Text("OK")))
+                    Alert(title: Text(alertTitle), message: Text(alertMsg),
+                          dismissButton: .default(Text("OK")) {
+                            self.nextQuestion()
+                    })
                 }
             }
         }
@@ -77,8 +83,16 @@ struct ContentView: View {
         enableAlert = true
     }
     
-    func showResult() {
-        
+    func nextQuestion() {
+        counter += 1
+        if counter < questionNum {
+            num1 = Int.random(in: 0...level)
+            num2 = Int.random(in: 0...level)
+        }
+        else {
+            counter = 0
+            settings = true
+        }
     }
 }
 
