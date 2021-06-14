ec6259761053268966a14c0547c1432f0780bc57
minusta <mu.yang@gmail.com>
2021-06-14 22:53:13 +0300

    add second view.

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -14,13 +14,29 @@ class User: ObservableObject {
 
 struct ContentView: View {
     @ObservedObject var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Show sheet") {
+            self.showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "@blucode")
+        }
     }
 }
 
+struct SecondView: View {
+    var name: String
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Text("Hello, \(name)!")
+        Button("Dismiss") {
+            self.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
