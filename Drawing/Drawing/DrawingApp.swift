4c232a98140aaea95df002e39e23b1d674a93e97
minusta <mu.yang@gmail.com>
2021-07-27 17:45:38 +0300

    Drawing: add triangle and arc.

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Mu Yang on 27.7.2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
