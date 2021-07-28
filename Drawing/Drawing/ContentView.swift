e82446cf8b5412ade72730650d6f0717922efa30
minusta <mu.yang@gmail.com>
2021-07-28 15:10:42 +0300

    Drawing: add strokeBoarder with image.

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
-        Triangle()
-        .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-        .frame(width: 200, height: 200)
+        VStack {
+            Flower(petaloffset: petalOffset, petalWidth: petalWidth)
+//                .stroke(Color.red, lineWidth: 1)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+            
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("punkaharju"), sourceRect: CGRect(x:0.5, y: 0.0, width: 0.6, height: 1), scale: 0.5), lineWidth: 20)
+                .frame(width: 300, height: 200)
+        }
         
-        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-            .stroke(Color.blue, lineWidth: 10)
-            .frame(width: 300, height: 300)
+//        Triangle()
+//        .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//        .frame(width: 200, height: 200)
+//        
+//        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+//            .stroke(Color.blue, lineWidth: 10)
+//            .frame(width: 300, height: 300)
     }
 }
 
@@ -45,6 +66,26 @@ struct Arc: Shape {
         var path = Path()
         path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: clockwise)
             
+        return path
+    }
+}
+
+struct Flower: Shape {
+    var petaloffset: Double = -20
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petaloffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
         return path
     }
 }
