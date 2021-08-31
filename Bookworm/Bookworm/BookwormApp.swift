5f2385dfb5a266580eb1a5681d9fe525775c224a
minusta <mu.yang@gmail.com>
2021-08-31 23:42:37 +0300

    Bookworm: AnyView

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Eric Yang on 28.8.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
