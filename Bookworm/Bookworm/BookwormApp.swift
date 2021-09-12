07db058fe0db8b6e8cae3749d1a895bbde882dcd
minusta <mu.yang@gmail.com>
2021-09-12 15:28:53 +0300

    BookWorm: use core data via .environment

--- Bookworm/Bookworm/BookwormApp.swift
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -13,8 +13,7 @@ struct BookwormApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            ContentView().environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
