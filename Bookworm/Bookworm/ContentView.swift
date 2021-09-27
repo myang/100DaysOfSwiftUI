0130ce13a18fb4a39ddfbc0febf3e2a47d6a5ec0
minusta <mu.yang@gmail.com>
2021-09-27 18:50:19 +0300

    Bookworm: add swap to delete.

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -10,8 +10,13 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var context
+    
     @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
-    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \Book.title, ascending: true),
+        NSSortDescriptor(keyPath: \Book.author, ascending: true)
+    ]) var books: FetchedResults<Book>
+    
     @State private var showingAddScreen = false
 
     var body: some View {
@@ -30,6 +35,16 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
+            }
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                self.showingAddScreen.toggle()
+            }) {
+                Image(systemName: "plus")
+            })
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView().environment(\.managedObjectContext, self.context)
             }
                 
             Button("Add") {
@@ -45,6 +60,15 @@ struct ContentView: View {
                 try? self.context.save()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            context.delete(book)
+        }
+        
+        try? context.save()
     }
 }
 
